9694dd79c26f476666e2122ec542f21893cab46b
Andriyann <50099343+Andriyann@users.noreply.github.com>
2020-09-25 20:20:37 -0700

    Add files via upload

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "andrian", quotes: "Whatever happens, keep love me" },
+    { author: "andrian", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "jawir", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "jawir", quotes: "I love you." },
+    { author: "jawir", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "jawir", quotes: "I love you." },
+    { author: "jawir", quotes: "I'm sorry for loving you." },
+];
---
+++ assets/stories.js
@@ -0,0 +1,6 @@
+export default [
+  "Ini Teks pertama",
+  "Ini Teks kedua",
+  "Ini Teks ke tiga ",
+  "Teks keempatnya, Aku sayang kamu!"
+];
